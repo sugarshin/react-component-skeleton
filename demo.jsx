5db696a2845566903aefefd15919f40096ee799c
sato <sato@tsumikiinc.com>
2015-03-20 13:05:17 +0900

    edit

--- demo.jsx
+++ demo.jsx
@@ -1,3 +1,4 @@
 "use strict";
 
+import from './react-component-skeleton.css'
 import ComponentName from './';
--- init.sh
+++ init.sh
@@ -3,4 +3,6 @@
 npm i -D babel babelify beefy browserify cssify
 npm i -S react
 mkdir lib
+echo '@charset "utf-8";\n\nbody {\n  background: indianred;\n}\n' > react-component-skeleton.css
 rm -rf .git init.sh
+npm run compile
