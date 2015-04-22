effa6912c84b70f9c7bc658b70d4fd1a4ab47341
sato <sato@tsumikiinc.com>
2015-04-22 16:57:24 +0900

    edit

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-npm i -D babel babelify browserify budo cssify garnish nib react stylus
+npm i -D babel babelify browserify budo cssify garnish nib react stylus watchify
 mkdir lib
 echo '@charset "utf-8"\n\nbody\n  background indianred\n' > src/react-component-skeleton.styl
 rm -rf .git init.sh
